8e0c25810186e5efbbb9509169a152c2e3896fc1
Mike Radka <mikeradka@gmail.com>
2025-03-02 08:31:24 -0500

    Create a module

---
+++ greetings/greetings.go
@@ -0,0 +1,11 @@
+package greetings
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+// 'Hello is the function name, 'name' is a parameter of type string, and the function returns a string.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
