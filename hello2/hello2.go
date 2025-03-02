30e8dda4a9ceb18caa6d103fb23c66398add0cce
Mike Radka <mikeradka@gmail.com>
2025-03-02 12:36:21 -0500

    Call the greetings module

---
+++ hello2/hello2.go
@@ -0,0 +1,14 @@
+// https://go.dev/doc/tutorial/call-module-code
+package main
+
+import (
+	"fmt"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
+}
