93525cd63b993960611517d2f8b4d4c9a6c750ab
Mike Radka <mikeradka@gmail.com>
2025-03-02 16:26:24 -0500

    Create a module, part 5 of 7: Return greetings for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,4 +1,4 @@
-// https://go.dev/doc/tutorial/random-greeting
+// https://go.dev/doc/tutorial/greetings-multiple-people
 
 package greetings
 
@@ -18,6 +18,26 @@ func Hello(name string) (string, error) {
 	// Create a message using a random format.
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+
+	return messages, nil
 }
 
 // randomFormat returns one of a set of greeting messages. The returned
