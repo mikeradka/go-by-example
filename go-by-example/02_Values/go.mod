c5ecbc32b063bab3ed7f2fcfb8f0a38a3a8750b8
Mike Radka <mikeradka@gmail.com>
2025-03-02 20:23:38 -0500

    Go by example: Values

---
+++ go-by-example/02_Values/go.mod
@@ -0,0 +1,3 @@
+module example.com/values
+
+go 1.24.0
---
+++ go-by-example/02_Values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang") // string
+
+	fmt.Println("1 + 1 =", 1+1)         // integer
+	fmt.Println("7.0 / 3.0 =", 7.0/3.0) // float
+
+	fmt.Println("true && false =", true && false) // boolean operators
+	fmt.Println("true || false =", true || false)
+	fmt.Println("!true =", !true)
+}
